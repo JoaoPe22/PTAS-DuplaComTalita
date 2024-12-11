8286cba81de3137fdf3d3a738cc0926ea7035ef5
JoaoPe22 <joao.canezin22@gmail.com>
2024-12-11 01:59:22 -0400

    Complemento da aula remota 04/12

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const prisma = require("./prisma/prismaClient");
 
 const express = require("express");
-const cors = require('cors')
+
+const AuthController = require("./controllers/AuthController");
+
+const cors = require("cors");
 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 
-
-//Responde a qualquer requisição encaminhada para 
+//Responde a qualquer requisição encaminhada para
 // /auth/algumaCoisa
 const authRoutes = require("./routes/authRoutes");
 
 //middleware//
-app.use("/auth",authRoutes);
+app.use("/auth", authRoutes);
+
+// Teste
+app.get("/privado", AuthController.verificaAutenticacao, (req, res) => {
+  res.json({
+    msg: "rota restrita",
+  });
+});
 
 app.listen(8000);
